c3c36c9bb7613ae31e386688ba6cf3c7ca4024ab
Gaurang <gaurangmali11@gmail.com>
2026-04-03 23:23:20 +0530

    Create Users Database table

---
+++ Backend (new)/src/main/java/com/example/CTube/model/User.java
@@ -0,0 +1,87 @@
+package com.example.CTube.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String passwordHash;
+
+    private String profilePicture;
+
+    private LocalDateTime joinDate;
+
+    private Integer subscriptionCount;
+    
+    
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public String getProfilePicture() {
+		return profilePicture;
+	}
+
+	public void setProfilePicture(String profilePicture) {
+		this.profilePicture = profilePicture;
+	}
+
+	public LocalDateTime getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(LocalDateTime joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public Integer getSubscriptionCount() {
+		return subscriptionCount;
+	}
+
+	public void setSubscriptionCount(Integer subscriptionCount) {
+		this.subscriptionCount = subscriptionCount;
+	}
+
+    
+    
+}
